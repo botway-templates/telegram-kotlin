b68f8a4783506ced7aaaaf72156930f2979721d4
abdfnx <abdfnabd@gmail.com>
2022-08-11 15:00:23 +0300

    init botway telegram kotlin template

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+
+    application
+}
+
+repositories {
+    mavenCentral()
+
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.google.guava:guava:30.1.1-jre")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.7")
+
+    implementation("net.mamoe.yamlkt:yamlkt:0.10.2")
+
+    implementation("com.beust:klaxon:5.5")
+}
+
+application {
+    mainClass.set("core.BotKt")
+}
---
+++ app/src/main/kotlin/core/Bot.kt
@@ -0,0 +1,51 @@
+package core
+
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.pollAnswer
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.polls.PollType.QUIZ
+import botway.*
+
+fun main() {
+    bot {
+        token = GetToken()
+        dispatch {
+            pollAnswer {
+                println("${pollAnswer.user.username} has selected the option ${pollAnswer.optionIds.lastOrNull()} in the poll ${pollAnswer.pollId}")
+            }
+            command("regularPoll") {
+                bot.sendPoll(
+                    chatId = ChatId.fromId(message.chat.id),
+                    question = "Pizza with or without pineapple?",
+                    options = listOf("With :(", "Without :)"),
+                    isAnonymous = false
+                )
+            }
+
+            command("quizPoll") {
+                bot.sendPoll(
+                    chatId = ChatId.fromId(message.chat.id),
+                    type = QUIZ,
+                    question = "Java or Kotlin?",
+                    options = listOf("Java", "Kotlin"),
+                    correctOptionId = 1,
+                    isAnonymous = false
+                )
+            }
+
+            command("closedPoll") {
+                bot.sendPoll(
+                    chatId = ChatId.fromId(message.chat.id),
+                    type = QUIZ,
+                    question = "Foo or Bar?",
+                    options = listOf("Foo", "Bar", "FooBar"),
+                    correctOptionId = 1,
+                    isClosed = true,
+                    explanation = "A closed quiz because I can"
+                )
+            }
+        }
+    }.startPolling()
+}
